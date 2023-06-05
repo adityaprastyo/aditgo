2f27310c691e2760e0ac137fcb1ce58586d6d59c
aditpras <aditya_prastyo@massindo.com>
2023-06-05 21:29:39 +0700

    Initial Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,34 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles =  Article.all
+  end
+
+  def show
+    if params[:id] == 'new'
+      @article = Article.new
+    else
+      @article = Article.find(params[:id])
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'projects/index'
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  root to: 'pages#index'
+
+  get 'resume/index'
+
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+
+  post 'articles', to: 'articles#create'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
